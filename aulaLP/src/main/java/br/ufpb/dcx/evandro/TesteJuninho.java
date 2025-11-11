bd50f0b176ecd206ce3bc2e48d3fa0691d363b94
evandro.lima <evandro.lima@dcx.ufpb.br>
2025-11-11 09:21:45 -0300

    Criando nova classe de teste usando IO

---
+++ aulaLP/src/main/java/br/ufpb/dcx/evandro/TesteJuninho.java
@@ -0,0 +1,9 @@
+package br.ufpb.dcx.evandro;
+
+public class TesteJuninho {
+    void main(){
+        IO.println("Oi");
+        String nome = IO.readln( "Digite seu nome: ");
+        IO.println("Oi " + nome);
+    }
+}
